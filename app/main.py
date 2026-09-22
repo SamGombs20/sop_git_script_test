f7cc2939cfb516e8eccb7f17d3136a1f7e8d3533
SamGombs20 <gombajosh18@gmail.com>
2026-09-22 15:00:29 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -17,3 +18,6 @@ async def crms():
 @app.get("/hello")
 async def hello():
     return {"hello": "world"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
